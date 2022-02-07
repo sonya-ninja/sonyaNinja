429a2fc07b1b2e939959204d460aaf8e7921e0c0
sonya-ninja <hello@sonya.ninja>
2022-02-07 18:01:57 +0200

    basic placeholder page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,34 @@
+<script>
+  import "../app.scss";
+</script>
+
+<video autoplay muted loop id="myVideo">
+	<source src="/video/sonya-ninja-web-development-animation.mp4" type="video/mp4">
+</video>
+<div class="overlay"></div>
+
+<slot />
+
+
+<style lang="scss">
+  video {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		object-fit: cover;
+		z-index: 1;
+	}
+	.overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: 2;
+		background-color: transparent;
+		background-image: linear-gradient(180deg, rgba(237,70,48,0.63) 0%, #ed5ea7 48%);
+		opacity: 1;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,161 +1,88 @@
 <script>
-	// PATCHING TOGETHER VARIOUS BITS FROM:
-	// https://note.com/npaka/n/naf87662867d1#Rggli
-	// https://svelte-cubed.vercel.app/docs/getting-started
-
-	// Nice walk cylce: https://tenor.com/view/ninja-girl-warrior-sexy-girl-sexy-ninja-gif-13464310
-
-
-	import { onMount } from 'svelte';
-	import * as THREE from 'three';
-	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-	import { VRM, VRMSchema } from '@pixiv/three-vrm';
-	import * as SC from 'svelte-cubed';
-
-	let height = 2;
-	let ninja;
-	let mixer;
-	let frame = 0;
-
-	SC.onFrame(() => {
-		frame += 0.01;
-		// console.log(frame);
-	});
-	
-
-	onMount(() => {
-		console.clear();
-		const loader = new GLTFLoader();
-
-		// Using VRM library with three.js GLTF loader for little ninja model created in Vroid Studio
-		// What is in the VRM schema?
-		console.log('VRM SCHEMA: ', VRMSchema);
-
-		loader.load(
-
-			// URL to the vroid model (VRM)
-			'/Sonya-Ninja.vrm',
-
-			// called when the resource is loaded
-			( gltf ) => {
-
-				// generate a VRM instance from gltf
-				VRM.from( gltf ).then( ( vrm ) => {
-
-					// what's in the vrm (Vroid model)?
-					console.log( 'vrm: ', vrm );
-
-					// THREE.JS Animation class:
-					// -AnimationClip : Has  animation timeline information.
-					// -AnimationAction  : Execution and status management of individual Animation Clips.
-					// -AnimationMixer : Manage and mix multiple Animation Actions.
-
-
-					// TESTING CONTROL THE vrm
-					// const head = vrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Head)
-					// head.rotation.x = Math.PI /6
-					// head.rotation.y = Math.PI /6
-					// vrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Angry, 1.0);
-					// vrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.I, 1.0);
-					// vrm.blendShapeProxy.update();
-
-
-					// generate a list of bones for the animation
-					const bones = [
-						VRMSchema.HumanoidBoneName.Head
-						].map((boneName) => {
-						return vrm.humanoid.getBoneNode(boneName)
-					})
-
-					//  generate timeline information about what to specify for the bone "position", "rotation", and "scale" values ​​at the number of milliseconds after the start of the animation
-					const clip = THREE.AnimationClip.parseAnimation({
-						hierarchy: [
-							{
-							keys: [
-								{
-								rot: new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)).toArray(),
-								time: 0                       
-								},
-								{
-								rot: new THREE.Quaternion().setFromEuler(new THREE.Euler(-40*Math.PI/180, 0, 0)).toArray(),
-								time: 1000                        
-								},
-								{
-								rot: new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)).toArray(),
-								time: 2000                        
-								}
-							]
-							}
-						]
-					}, bones)
-
-					// When using Animation Clip with VRM, you need to change the track name
-					clip.tracks.some((track) => {
-						track.name = track.name.replace(/^\.bones\[([^\]]+)\].(position|quaternion|scale)$/, '$1.$2')
-					})
-
-					mixer = new THREE.AnimationMixer(vrm.scene);
-					if (mixer) {
-						mixer.update(frame);
-					};
-					let action = mixer.clipAction(clip);
-					action.play();
-
-					// We're using svelte-cubed to put the vroid scene in the canvas
-					// Let's call her Ninja, rotate her and add her shadow
-					ninja = vrm.scene;
-					ninja.rotation.y = Math.PI;
-					ninja.traverse(c => {
-						c.castShadow = true;
-					})
-
-				} );
-
-			},
-
-			// called while loading is progressing
-			( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
-
-			// called when loading has errors
-			( error ) => console.error( error )
-
-		);
-
-	});
-	
+	import Logo from "$lib/svg/Logo.svelte";
 </script>
 
-<SC.Canvas antialias background={new THREE.Color('papayawhip')} fog={new THREE.FogExp2('papayawhip', 0.1)} shadows>
 
-	<!-- <SC.Mesh geometry={new THREE.BoxGeometry()} material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })} castShadow /> -->
-	<SC.Primitive object={ninja} position={[0, -height / 2, 0]} />
-
-	<SC.PerspectiveCamera position={[1, 1, 2]} />
-	<SC.OrbitControls enableZoom={true} maxPolarAngle={Math.PI * 0.51} />
-	<SC.AmbientLight intensity={0.3} />
-	<SC.DirectionalLight intensity={0.6} position={[2, 3, 2]} shadow={{ mapSize: [4048, 4048] }} />
-
-	<SC.Group position={[0, -height / 2, 0]}>
-		<SC.Mesh geometry={new THREE.PlaneGeometry(50, 50)} material={new THREE.MeshStandardMaterial({ color: 'pink' })} rotation={[-Math.PI / 2, 0, 0]} receiveShadow />
-		<SC.Primitive object={new THREE.GridHelper(50, 50, 0x444444, 0x555555)} position={[0, 0.001, 0]} />
-	</SC.Group>
-</SC.Canvas>
 
 <div class="pageWrap">
 	<header>
-		<h1>Sonya Ninja</h1>
-		<p>Welcome!</p>
+		<Logo />
 	</header>
+	<section>
+		<h1>Kick Ass Web Development</h1>
+		<p class="contact">hello@sonya.ninja</p>
+		<ul>
+			<li>Web Apps</li>
+			<li>Websites</li>
+			<li>Hosting</li>
+			<li>SEO</li>
+			<li>UX and UI</li>
+			<li>Graphic Design</li>
+			<li>Social Media Management</li>
+		</ul>
+	</section>
 </div>
 
 
-<style>
+<style lang="scss">
 	.pageWrap {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 50vw;
-		height: 50vh;
-		z-index: 2;
+		z-index: 10;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+			header {
+			width: 100vw;
+			height: 100vh;
+			display: grid;
+			grid-template-columns: 1fr;
+			grid-template-rows: 1fr;
+			justify-items: center;
+			align-items: center;
+		}
+		:global(header svg) {
+			grid-column: 1;
+			grid-row: 1;
+			width: clamp(200px, 90%, 800px);
+		}
+		section {
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			margin: 6vw;
+		}
+		h1 {
+			text-align: center;
+			margin: 0 0 1rem 0;
+			font-size: 3rem;
+			color: rgba(0,0,0,0.9);
+			font-weight: 500;
+		}
+		.contact {
+			margin: 0 0 3rem 0;
+			font-weight: 400;
+			font-size: 2rem;
+		}
+		ul {
+			font-size: 1.3rem;
+			padding: 0;
+			margin: 0;
+			list-style: none;
+			display: flex;
+			flex-direction: column;
+			li {
+				text-align: center;
+				padding: 0.3rem 0;
+			}
+		}
 	}
+
+	// @media (min-width: 600px) {
+	// 	:global(header svg) {
+	// 		width: 50vw;
+	// 	}
+	// }
+	
+
 </style>
